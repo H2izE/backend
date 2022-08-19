c8fcf8bdd1154955b1d9e23bc7e4898a55c832f5
jangsujeong <sujeongjang1214@gmail.com>
2022-08-20 00:30:08 +0900

    4. js-scroll event

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
 
-
 const heart = document.querySelector('.heart_btn');//단독실행하면 안됨
+const header = document.querySelector('#header');
 
 heart.addEventListener('click', function () {
     console.log('하트누름 정상작동');
     heart.classList.toggle('on');
 });
+
+function scrollFunc() {
+    console.log(pageYOffset);
+    if (pageYOffset >= 10) {
+        header.classList.add('on');
+    } else {
+        header.classList.remove('on');
+    }
+}
+
+window.addEventListener('scroll', scrollFunc);
